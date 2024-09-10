e064ac73f1c0caf9884a048773331d6f0a54af6e
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2024-09-10 16:49:09 +0700

    fix

--- src/components/accordion/accordion-item.jsx
+++ src/components/accordion/accordion-item.jsx
@@ -8,8 +8,10 @@ const AccordionItem = ({ question, answer, isOpen, onClick }) => {
   return (
     <div className="wrapper">
       <button className={`question-container ${isOpen ? "active" : ""}`} onClick={onClick}>
-        <p className="question-content text-[4vw] sm:text-xl">{question}</p>
-        <ChevronDownIcon className={`arrow w-6 h-6 ${isOpen ? "active" : ""}`} />
+          <p className="question-content text-[4vw] sm:text-xl pr-1">{question}</p>
+          <div className="w-6 h-6">
+              <ChevronDownIcon className={`arrow w-6 h-6 ${isOpen ? "active" : ""}`} />
+          </div>
       </button>
 
       <div
